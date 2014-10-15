9e14d3d89c46c5a7f6e07e0529cf10047c913dfd
nickjdavis <njdavis@gmail.com>
2014-10-15 22:05:33 +0100

    Fixed warping! Not very robust - check directory.

--- ScalpGM_warpMNI.m
+++ ScalpGM_warpMNI.m
@@ -1,52 +1,81 @@
 function mnifile = ScalpGM_warpMNI (T1file,distfile)
 
-% Dvol = spm_vol(distfile);
-% Dimg = spm_read_vols(Dvol);
 
-% ref = 'C:\Program Files\MATLAB\spm12b\canonical\single_subj_t1.nii';
+%% Step 1 - "Normalise: Est" T1 to template to get y_ file
+% spm_jobman('initcfg');
+% % matlabbatch{1}.spm.spatial.normalise.est.subj.vol = {'H:\Documents\Git\ScalpGM\data\sHIVE4-0301-00003-000001-01.img,1'};
+% matlabbatch{1}.spm.spatial.normalise.est.subj.vol = {T1file};
+% matlabbatch{1}.spm.spatial.normalise.est.eoptions.biasreg = 0.0001;
+% matlabbatch{1}.spm.spatial.normalise.est.eoptions.biasfwhm = 60;
+% matlabbatch{1}.spm.spatial.normalise.est.eoptions.tpm = {'C:\Program Files\MATLAB\spm12b\tpm\TPM.nii'};
+% matlabbatch{1}.spm.spatial.normalise.est.eoptions.affreg = 'mni';
+% matlabbatch{1}.spm.spatial.normalise.est.eoptions.reg = [0 0.001 0.5 0.05 0.2];
+% matlabbatch{1}.spm.spatial.normalise.est.eoptions.fwhm = 0;
+% matlabbatch{1}.spm.spatial.normalise.est.eoptions.samp = 3;
+% spm_jobman('run',matlabbatch);
+% 
+% disp('Done normest')
 
-% Step 1 - "Normalise: Est" T1 to template to get y_ file
-spm_jobman('initcfg');
-% matlabbatch{1}.spm.spatial.normalise.est.subj.vol = {'H:\Documents\Git\ScalpGM\data\sHIVE4-0301-00003-000001-01.img,1'};
-matlabbatch{1}.spm.spatial.normalise.est.subj.vol = {T1file};
-matlabbatch{1}.spm.spatial.normalise.est.eoptions.biasreg = 0.0001;
-matlabbatch{1}.spm.spatial.normalise.est.eoptions.biasfwhm = 60;
-matlabbatch{1}.spm.spatial.normalise.est.eoptions.tpm = {'C:\Program Files\MATLAB\spm12b\tpm\TPM.nii'};
-matlabbatch{1}.spm.spatial.normalise.est.eoptions.affreg = 'mni';
-matlabbatch{1}.spm.spatial.normalise.est.eoptions.reg = [0 0.001 0.5 0.05 0.2];
-matlabbatch{1}.spm.spatial.normalise.est.eoptions.fwhm = 0;
-matlabbatch{1}.spm.spatial.normalise.est.eoptions.samp = 3;
-spm_jobman('run',matlabbatch);
 
-disp('Done normest')
 
+%% Step 2 - "Normalise: Write" to warp distimage into template
 %%% NB!!! Message in workspace...
 % Item normalise: No field(s) named
 % write
-%%% CHECK!!!
-
-
-% Step 2 - "Normalise: Write" to warp distimage into template
+%%% CHECK!!! NB when does output file get specified? Need to check!
 yfile = ['y_' T1file]; % CHECK!
 spm_jobman('initcfg');
-% matlabbatch{1}.spm.spatial.normalise.write.subj.def = {'H:\Documents\Git\ScalpGM\data\y_sHIVE4-0301-00003-000001-01.nii'};
-% matlabbatch{1}.spm.spatial.normalise.write.subj.resample = {'H:\Documents\Git\ScalpGM\data\dc1sHIVE4-0301-00003-000001-01.nii,1'};
+%{
 matlabbatch{1}.spm.spatial.normalise.write.subj.def = {yfile};
 matlabbatch{1}.spm.spatial.normalise.write.subj.resample = {distfile};
 matlabbatch{1}.spm.spatial.normalise.write.woptions.bb = [-78 -112 -70
                                                           78 76 85];
 matlabbatch{1}.spm.spatial.normalise.write.woptions.vox = [2 2 2];
 matlabbatch{1}.spm.spatial.normalise.write.woptions.interp = 4;
+matlabbatch{1}.spm.spatial.normalise.write.roptions.prefix = 'w';
 spm_jobman('run',matlabbatch);
 
-% TODO - this bit is worng - need to get fileparts.
+%}
+
+% matlabbatch{1}.spm.spatial.normalise.estwrite.subj.vol = {'C:\Users\psdavinj\Documents\MATLAB\ScalpGM\ScalpGM\data\sHIVE4-0301-00003-000001-01.img,1'};
+% matlabbatch{1}.spm.spatial.normalise.estwrite.subj.resample = {'C:\Users\psdavinj\Documents\MATLAB\ScalpGM\ScalpGM\data\dc1sHIVE4-0301-00003-000001-01.nii,1'};
+matlabbatch{1}.spm.spatial.normalise.estwrite.subj.vol = {T1file};
+matlabbatch{1}.spm.spatial.normalise.estwrite.subj.resample = {distfile};
+matlabbatch{1}.spm.spatial.normalise.estwrite.eoptions.biasreg = 0.0001;
+matlabbatch{1}.spm.spatial.normalise.estwrite.eoptions.biasfwhm = 60;
+matlabbatch{1}.spm.spatial.normalise.estwrite.eoptions.tpm = {'C:\Program Files\MATLAB\spm12b\tpm\TPM.nii'};
+matlabbatch{1}.spm.spatial.normalise.estwrite.eoptions.affreg = 'mni';
+matlabbatch{1}.spm.spatial.normalise.estwrite.eoptions.reg = [0 0.001 0.5 0.05 0.2];
+matlabbatch{1}.spm.spatial.normalise.estwrite.eoptions.fwhm = 0;
+matlabbatch{1}.spm.spatial.normalise.estwrite.eoptions.samp = 3;
+matlabbatch{1}.spm.spatial.normalise.estwrite.woptions.bb = [-78 -112 -70
+                                                             78 76 85];
+matlabbatch{1}.spm.spatial.normalise.estwrite.woptions.vox = [2 2 2];
+matlabbatch{1}.spm.spatial.normalise.estwrite.woptions.interp = 4;
+spm_jobman('run',matlabbatch);
+
+% TODO - check it conforms to output file
 [pathstr,name,ext] = fileparts(distfile);
-mnifile = [pathstr '\' 'w' name ext];
+mnifile = ['w' name ext];
 
 disp('Done normwrite')
 
 
 
+
+
+%% SPARE CODE
+
+% WRITE EXAMPLE SPM12b
+% Normalise: Write
+%--------------------------------------------------------------------------
+% % matlabbatch{4}.spm.spatial.normalise.write.subj.def      = cellstr(spm_file(a,'prefix','y_','ext','nii'));
+% % matlabbatch{4}.spm.spatial.normalise.write.subj.resample = cellstr(f);
+% % matlabbatch{4}.spm.spatial.normalise.write.woptions.vox  = [3 3 3];
+% % 
+% % matlabbatch{5}.spm.spatial.normalise.write.subj.def      = cellstr(spm_file(a,'prefix','y_','ext','nii'));
+% % matlabbatch{5}.spm.spatial.normalise.write.subj.resample = cellstr(spm_file(a,'prefix','m','ext','nii'));
+% % matlabbatch{5}.spm.spatial.normalise.write.woptions.vox  = [1 1 3];
 
 
 
