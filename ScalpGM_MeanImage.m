56e7e2edc85014a5469112951292a4c889adef1f
nickjdavis <njdavis@gmail.com>
2015-08-18 00:53:35 +0100

    Updated meanimage code. Need to (a) tidy and (b) protect against NaNs.

--- ScalpGM_MeanImage.m
+++ ScalpGM_MeanImage.m
@@ -1,9 +1,14 @@
+% TODO
+% 1. Tidy up
+% 2. Chase NaNs in output
+
 function ScalpGM_MeanImage (filelist)
 
 nFiles = length(filelist);
 
 Msum = zeros(79,95,79); % Sum of valid voxel values
 Mvox = zeros(79,95,79); % No of valid voxels per division
+V = zeros(79,95,79,nFiles); % All voxels, ready for SD
 
 for i=1:nFiles
     % import file
@@ -19,6 +24,7 @@ for i=1:nFiles
         if length(r)>2
             Mvox(r,c,z) = Mvox(r,c,z)+1;
             Msum(r,c,z) = Msum(r,c,z)+Dimg(r,c);
+            V(r,c,z,i)  = Dimg(r,c);
         end
         % %         CH = convhull(r,c);
         % %         %SCallpoints = [SCallpoints; r c i*ones(length(r),1)];
@@ -42,25 +48,52 @@ end
 % end
 % end
 
-% mean image is num./den
-disp('Writing mean image')
-%%%M = Mnum./Msum;
-M = Msum ./ Mvox;
-% size(M)
-% plot3(M(:,1),M(:,2),M(:,3),'.')
-% save mean image
+% % mean image is num./den
+% disp('Writing mean image')
+% %%%M = Mnum./Msum;
+% M = Msum ./ Mvox;
+% % size(M)
+% % plot3(M(:,1),M(:,2),M(:,3),'.')
+% % save mean image
+% Mvol = Dvol;
+% outName = 'meanimage_new.nii';
+% Mvol.fname = outName;
+% spm_write_vol(Mvol,M);
+
+% Alt version of mean image - use V
+disp('Alternative mean image')
+outName = 'meanimage_alt.nii';
+Vm = zeros(79,95,79);
+Vm(:,:,:) = mean (V,4);
 Mvol = Dvol;
-outName = 'meanimage_new.nii';
 Mvol.fname = outName;
-spm_write_vol(Mvol,M);
+spm_write_vol(Mvol,Vm);
+% Standard deviation image - use V
+disp('Alternative SD image')
+outName = 'meanimage_alt_sd.nii';
+Sm = zeros(79,95,79);
+Sm(:,:,:) = std (V,0,4);
+Mvol = Dvol;
+Mvol.fname = outName;
+spm_write_vol(Mvol,Sm);
+% Coefficient of variation image - SD/mean
+disp('CoV image')
+outName = 'meanimage_alt_cov.nii';
+%Cm = zeros(79,95,79);
+Cm = Sm./Vm;
+Mvol = Dvol;
+Mvol.fname = outName;
+spm_write_vol(Mvol,Cm);
+N = size(find(isnan(Cm)==1))
 
 
-M(isinf(M))=NaN;
-M(M>5)=NaN;
-M(M<.1)=NaN;
-MM = M(~isnan(M));
-% MM(MM>255)=255;
-min(MM)
-max(MM)
-
-hist(MM,100)
+% 
+% M(isinf(M))=NaN;
+% M(M>5)=NaN;
+% M(M<.1)=NaN;
+% MM = M(~isnan(M));
+% % MM(MM>255)=255;
+% min(MM)
+% max(MM)
+% 
+% hist(MM,100)
