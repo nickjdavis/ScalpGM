705e81e1b1fa7a2354944f204c48efc11e27cc6d
nickjdavis <njdavis@gmail.com>
2017-06-30 17:23:39 +0100

    Snowdon: _TestSliceDistance needs dist calc

--- ScalpGM_TestSliceDistance.m
+++ ScalpGM_TestSliceDistance.m
@@ -6,19 +6,46 @@ function ScalpGM_TestSliceDistance ()
 T1 = '.\data3\HIVEx.img';
 GM = '.\data3\c1HIVEx.nii';
 SC = '.\data3\c5HIVEx.nii';
+z  = 128;  % z-level for slice
+th = 0.95; % Threshold for scalp/GM mask
 
 % Get slice from files
-T1slice = [];
-GMslice = [];
-SCslice = [];
+T1data = spm_read_vols (spm_vol(T1));
+T1slice = T1data(:,:,z);
+GMdata = spm_read_vols (spm_vol(GM));
+GMslice = GMdata(:,:,z);
+SCdata = spm_read_vols (spm_vol(SC));
+SCslice = SCdata(:,:,z);
+GMmask = imbinarize(GMslice,th);
+SCmask = bwareafilt(imbinarize(SCslice,th),1);
 
 % plot for sanity
+% image(T1slice, 'CDataMapping','scaled'); colorbar
+% waitforbuttonpress
+% % image(GMslice, 'CDataMapping','scaled');  colorbar
+% % waitforbuttonpress
+% image(SCslice, 'CDataMapping','scaled'); colorbar
+% waitforbuttonpress
+image(SCmask, 'CDataMapping','scaled'); colorbar
+waitforbuttonpress
+
 
 % get convex hull of SCslice
-
+B = bwboundaries (SCmask, 'noholes');
+SCboundary = B{1};
 % plot convex hull
+hold on;
+plot (SCboundary(:,2),SCboundary(:,1),'r','LineWidth',2)
+
 
 % get voxels from GM
+G = find(GMmask);
+for i=1:10
+    v = GMmask(ind2sub(GMmask,i))
+    distvec = sqrt( (SCboundary(:,1)-v(1)).^2 + (SCboundary(:,2)-v(2)).^2 ); % + (scalp_points(:,3)-z).^2);
+    [d,pos] = min( distvec )
+end
+
 
 % Step through voxels, getting nearest point on CH
 % Draw line between GM voxel and CH point
