2a407604330aebfe6f0fe2e208745bcbc74eda8f
nickjdavis <njdavis@gmail.com>
2018-11-05 14:18:24 +0000

    plot two file added

---
+++ plottwobrains.m
@@ -0,0 +1,9 @@
+% Load two brain images
+% Warp both to MNI
+% 3d plot both in diff colours
+
+imgX = '\\staffhome\staff_home0\55121576\Documents\MATLAB\ScalpGM\ScalpGM\data3\original\HIVEx.img';
+imgY = '\\staffhome\staff_home0\55121576\Documents\MATLAB\ScalpGM\ScalpGM\data3\original\HIVEy.img';
+
+
+
