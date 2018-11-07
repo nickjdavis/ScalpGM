081647bbeb9a2f33c023362739f7ee2aa946191e
nickjdavis <njdavis@gmail.com>
2018-11-07 11:38:43 +0000

    Working on 3d code

--- ScalpGM_Distance.m
+++ ScalpGM_Distance.m
@@ -34,32 +34,32 @@ distfile = outName;
 S = size(Dimg);
 
 %% Calculate distances
-% for z=1:GMvol.dim(3)
-%     for y=1:GMvol.dim(2)
-%         for x=1:GMvol.dim(1)
-%             if GMmask(x,y,z)==1
-%                 % voxel==1 so get dist and write
-%                 %distvec = sqrt( (scalp_points(:,1)-GMmask(i,1)).^2 + (scalp_points(:,2)-GM(i,2)).^2 );
-%                 distvec = sqrt( (scalp_points(:,1)-x).^2 + (scalp_points(:,2)-y).^2 + (scalp_points(:,3)-z).^2);
-%                 [d,pos] = min( distvec );
-%                 Dimg(x,y,z) = d/100;
-%             end
-%         end
-%     end
-% end
-
-I = find(GMimg>0.9);
-D = zeros(length(I),2);
-%parfor i=1:length(I)
- for i=1:length(I)
-    % convert back to coordinate x,y,z
-    [x,y,z] = ind2sub(S,I(i));
-    % do distvec on it
-    distvec = sqrt( (scalp_points(:,1)-x).^2 + (scalp_points(:,2)-y).^2 + (scalp_points(:,3)-z).^2);
-    [d,pos] = min( distvec );
-    %Dimg(I(i)) = d/100;
-    D(i,:) = [I(i) d/100];
+for z=1:GMvol.dim(3)
+    for y=1:GMvol.dim(2)
+        for x=1:GMvol.dim(1)
+            if GMmask(x,y,z)==1
+                % voxel==1 so get dist and write
+                %distvec = sqrt( (scalp_points(:,1)-GMmask(i,1)).^2 + (scalp_points(:,2)-GM(i,2)).^2 );
+                distvec = sqrt( (scalp_points(:,1)-x).^2 + (scalp_points(:,2)-y).^2 + (scalp_points(:,3)-z).^2);
+                [d,pos] = min( distvec );
+                Dimg(x,y,z) = d;
+            end
+        end
+    end
 end
+
+% I = find(GMimg>0.9);
+% D = zeros(length(I),2);
+% %parfor i=1:length(I)
+%  for i=1:length(I)
+%     % convert back to coordinate x,y,z
+%     [x,y,z] = ind2sub(S,I(i));
+%     % do distvec on it
+%     distvec = sqrt( (scalp_points(:,1)-x).^2 + (scalp_points(:,2)-y).^2 + (scalp_points(:,3)-z).^2);
+%     [d,pos] = min( distvec );
+%     %Dimg(I(i)) = d/100;
+%     D(i,:) = [I(i) d];%/100]; % !! removed div by 100
+% end
 
 Dimg(D(:,1))=D(:,2);
 
--- plottwobrains.m
+++ plottwobrains.m
@@ -18,6 +18,11 @@ for i=1:2
     disp(['-- Scalp file : ' scalpfile])
     disp(['-- Grey matter: ' gmfile])
     scalp_points = ScalpGM_getCH3d (scalpfile);
+    % TODO - not sure what _Distance is returning - seems to be distance
+    % from a single point in space. First try NOT dividing distance by 100,
+    % then try stepping through the logic of the distvec calculation
+    % HMMM - Maybe the problem is in scalp_points? The numbers don't make
+    % sense. Try stepping through this...?
     distfile = ScalpGM_Distance (scalp_points,gmfile);
     [mnifile,yfile] = ScalpGM_warpMNI (T1file,distfile,TPMfile);
     M{i} = mnifile
@@ -25,8 +30,8 @@ for i=1:2
 end
 
 % HOORAY!!!!
-% The two images prooduced afetr warping overlay beautifully!
+% The two images produced after warping overlay beautifully!
 % So the 'mnifile' truly is in MNI space.
 % TODO: Now work out what the source image is - colours/values
-% don't look right for Scamlp-GM distance, so may need to use 
+% don't look right for Scalp-GM distance, so may need to use 
 % the y-file to warp the image at the next stage.
