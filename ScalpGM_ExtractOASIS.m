d109fd84260565161d278a0188e52b69e210b5e2
nickjdavis <njdavis@gmail.com>
2018-12-21 09:43:59 +0000

    Editing _ExtractOASIS to handle collecting and reorienting images

--- ScalpGM_ExtractOASIS.m
+++ ScalpGM_ExtractOASIS.m
@@ -1,14 +1,30 @@
 % Extract OASIS brains from folders
-% Currently converts to NIFTI and separates files
-% TODO: Return list of files? Location?
+% - uses oasis_reorient.m by Ged Ridgway
+% - transforms files to SPM-aligned space then returns list
 
-function ScalpGM_ExtractOASIS (folder)
+function F = ScalpGM_ExtractOASIS (folder, copyto)
+
+if nargin<2
+    copyto = '';
+end
 
 dirin = cd;
 cd (folder)
 d = dir('*.img');
 n = length(d);
 
+F = []; for i=1:n
+    F = [F; d(i).name];
+end
+reorient_oasis(F);
+
+
+if ~strcmp(copyto, '')
+    % copy file(s) to new folder
+end
+
+%{
+% OLD - trying to manually reorient
 for i=1:n
     % open hdr
     f = d(i).name;
@@ -25,29 +41,29 @@ for i=1:n
     spm_write_vol(V,ima);
 
 end
+%}
 
-% Create new subfolders for OASIS and for NII
-try
-    mkdir('NII');
-    mkdir('OASIS');
-catch
-    % issues a warning if dirs exist
-    % todo - suppress warning, or check for dir
-end
-% Move files
-s = movefile('*.nii','NII');
-if s~=1
-    disp('Move error: NII');
-end
-s = movefile('*.hdr','OASIS');
-if s~=1
-    disp('Move error: HDR');
-end
-s = movefile('*.img','OASIS');
-if s~=1
-    disp('Move error: IMG');
-end
-
+% % Create new subfolders for OASIS and for NII
+% try
+%     mkdir('NII');
+%     mkdir('OASIS');
+% catch
+%     % issues a warning if dirs exist
+%     % todo - suppress warning, or check for dir
+% end
+% % Move files
+% s = movefile('*.nii','NII');
+% if s~=1
+%     disp('Move error: NII');
+% end
+% s = movefile('*.hdr','OASIS');
+% if s~=1
+%     disp('Move error: HDR');
+% end
+% s = movefile('*.img','OASIS');
+% if s~=1
+%     disp('Move error: IMG');
+% end
 
 
 cd(dirin)
