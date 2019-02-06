bfaa2358297bec3bdb2e09b8fe41a8153959ad35
nickjdavis <njdavis@gmail.com>
2019-02-06 01:25:14 +0000

    Added TODOs for handling the tables and removing the log file

--- ScalpGM.m
+++ ScalpGM.m
@@ -17,19 +17,13 @@ if nargin<2
     benchmark=0;
 end
 
-% dirin = cd();
-% cd (folder);
-% % file mask for scans
-% % d = dir ([folder '\*.img']);
-% %d = dir ('*.img');
-% d = dir('*.nii');
-% n = length(d);
 
 % readtable
 T = readtable(logfile);
 I = T.imgfile;
 n = length(I);
 
+% TODO - new table columns, and create new table
 
 % Link to TPM file
 % TODO - get this rel to SPM path
@@ -54,12 +48,7 @@ for i=1:n
     T1file = I{i}; %d(i).name;
     [pathstr,fname,ext] = fileparts(T1file);
     fprintf('Processing file %d of %d : %s',i,n,fname)
-    % OASIS brains need to be be rescaled
-    %uT1file = nii_unity(T1file);
-    %snfile = ScalpGM_getSN (T1file);
-    % segment image
     try
-        % [scalpfile, gmfile] = ScalpGM_segmentImage (T1file);
         [scalpfile, gmfile] = ScalpGM_segmentImage (T1file,TPMfile);
         disp(['-- Scalp file : ' scalpfile])
         disp(['-- Grey matter: ' gmfile])
@@ -68,12 +57,6 @@ for i=1:n
         %     scalp_points = ScalpGM_getCH (scalpfile);
         scalp_points = ScalpGM_getCH3d (strcat(pathstr,'\',scalpfile));
         toc2=toc;
-        %disp(['-- CH file    : ' scalp_points])
-        % TODO: smooth convex hull
-        % calculate scalp-GM distance
-        %if (isPar)
-        %    distfile = ScalpGM_Distance_par (scalp_points,gmfile);
-        %else
             distfile = ScalpGM_Distance (scalp_points,strcat(pathstr,'\',gmfile));
         %end
         toc3=toc;
@@ -84,6 +67,8 @@ for i=1:n
         toc4=toc;
         disp(['-- MNI file   : ' mnifile])
         
+        % TODO - compile filenames into new table
+        
         % output???
         if benchmark==1
             fprintf('Total elapsed time: %4.2f\nSegment  : %4.2f sec\nConvHull : %4.2f\nDistance : %4.2f\nMNI warp : %4.2f\n\n',...
@@ -92,34 +77,33 @@ for i=1:n
         
         % write log file
         % NB this is written in the target directory
-        disp('-- writing log file')
-        logfile = 'ScalpGM_log.txt';
-        logstr = sprintf('%s\t%s\t%s\t%s\t%s\t%s\t%s\n',datestr(now),...
-            T1file, scalpfile, gmfile, distfile, mnifile,yfile);
-        fid = fopen(logfile,'a');
-        fprintf(fid,'%s',logstr);
-        fclose(fid);
-        disp('-- closing log file')
+%         disp('-- writing log file')
+%         logfile = 'ScalpGM_log.txt';
+%         logstr = sprintf('%s\t%s\t%s\t%s\t%s\t%s\t%s\n',datestr(now),...
+%             T1file, scalpfile, gmfile, distfile, mnifile,yfile);
+%         fid = fopen(logfile,'a');
+%         fprintf(fid,'%s',logstr);
+%         fclose(fid);
+%         disp('-- closing log file')
+
+
     catch
-        disp('-- writing log file (fail)')
-        logfile = 'ScalpGM_log.txt';
-        logstr = sprintf('%s\t%s\t%s\t%s\t%s\t%s\t%s\n',datestr(now),...
-            T1file, 'fail', 'fail', 'fail', 'fail','fail');
-        fid = fopen(logfile,'a');
-        fprintf(fid,'%s',logstr);
-        fclose(fid);
-        disp('-- closing log file (fail)')
+        % TODO - add 'fail' to table for output
+       
+%         disp('-- writing log file (fail)')
+%         logfile = 'ScalpGM_log.txt';
+%         logstr = sprintf('%s\t%s\t%s\t%s\t%s\t%s\t%s\n',datestr(now),...
+%             T1file, 'fail', 'fail', 'fail', 'fail','fail');
+%         fid = fopen(logfile,'a');
+%         fprintf(fid,'%s',logstr);
+%         fclose(fid);
+%         disp('-- closing log file (fail)')
         disp(lasterr)
     end
     %plot3 (scalp_points(:,1),scalp_points(:,2),scalp_points(:,3),'.')
 end
 
 
+% TODO - writetable
 
 
-
-% clean up and exit
-% cd (dirin)
-% if isPar
-%     delete(pool);
-% end
