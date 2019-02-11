4885305b64b07b0b2b242ca7252844cfaba9686b
nickjdavis <njdavis@gmail.com>
2019-02-11 18:38:11 +0000

    Test_GMSc plots two images (GM and dist) next to each other. TODO: Separate colormaps?

--- ScalpGM_TestGMSc.m
+++ ScalpGM_TestGMSc.m
@@ -9,13 +9,20 @@ for i=1:nfiles
     t = T(i,:);
     [p,n,e] = fileparts(t.imgfile{:});
     % open scalp file
-    sc = ScalpGM_getCH3d (strcat(p,'\',t.scalp{:}));
+    %sc = ScalpGM_getCH3d (strcat(p,'\',t.scalp{:}));
     % open GM file
-    gm = spm_vol(strcat(p,'\',t.GM{:}))
+    gm = spm_vol(strcat(p,'\',t.GM{:}));
+    di = spm_vol(strcat(p,'\',t.dist{:}));
     % plot mesh over GM
 %     plot3(sc(:,1),sc(:,2),sc(:,3),'.')
 %     waitforbuttonpress
     X = gm.private.dat;
-    image(X(:,:,100))
+    Y = di.private.dat;
+    subplot(1,2,1)
+    imagesc(X(:,:,100)); colormap(gca,'gray');
+    nn = strrep(t.GM{:},'_','\_'); title(nn)
+    subplot(1,2,2)
+    imagesc(Y(:,:,100)); colormap(gca,'jet');
+    nn = strrep(t.dist{:},'_','\_'); title(nn)
     waitforbuttonpress
 end
