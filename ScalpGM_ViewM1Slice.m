963ab6c3aeb310b3e5a8b710be24220c5400159d
nickjdavis <njdavis@gmail.com>
2019-06-04 20:29:37 +0100

    _ViewM1Slice works very simply. Need to test on MNI images

--- ScalpGM_ViewM1Slice.m
+++ ScalpGM_ViewM1Slice.m
@@ -2,6 +2,22 @@
 %
 % Loads a single slice at around M1 level
 % displays SGM distance
+%
+% MNI: Left M1 [-48 -8 54], Right M1 [52 -4 50]
+
 
 function ScalpGM_ViewM1Slice (imagefile)
+
+% open file
+im = spm_vol(imagefile);
+imdata = im.private.dat;
+size(imdata)
+
+% select slice
+X = squeeze(imdata(:,120,:));
+size(X)
+
+% display
+imagesc(rot90(X)); colorbar;
+
 
