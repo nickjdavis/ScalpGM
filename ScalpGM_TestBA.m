9bc8fd2cc36681fbfe64d167ce35b97cbd062809
nickjdavis <njdavis@gmail.com>
2019-07-06 07:45:36 +0100

    Changes to _TestBA

--- ScalpGM_TestBA.m
+++ ScalpGM_TestBA.m
@@ -1,19 +1,38 @@
 function DistbyBA = ScalpGM_TestBA (filelist)
 
+%% Load atlas
+Atlas = spm_read_vols(spm_vol('brodmann.nii'));
+BA1 = 4;  % primary motor cortex
+BA2 = 17; % primary visual cortex
+BA3 = 27; % piriform cortex
+BA4 = 46; % dlpfc
+% Extract Left M1, Left V1, Left PC
+% NB L hemi has x<92
+BAmask1 = ismember(Atlas,BA1); BAmask1(92:end,:,:)=false;
+BAmask2 = ismember(Atlas,BA2); BAmask2(92:end,:,:)=false;
+BAmask3 = ismember(Atlas,BA3); BAmask3(92:end,:,:)=false;
+BAmask4 = ismember(Atlas,BA4); BAmask4(92:end,:,:)=false;
+
+
 %% Read list of files
-    T = readtable(filelist);
-    nFiles = size(T,1);
-    F = {};
-    D = T.imgfolder;
-    I = T.imgfile;
-    M = T.MNI;
-    for i=1:nFiles
-        p = D{i};
-        % get MNI file
-        f=strcat(p,'\',M{i});
-        % add folder+mni to F
-        F=[F;f];
-    end
+T = readtable(filelist);
+nFiles = size(T,1);
+F = {};
+D = T.imgfolder;
+I = T.imgfile;
+M = T.MNI;
+for i=1:nFiles
+    p = D{i};
+    % get MNI file
+    f=strcat(p,'\',M{i});
+    % add folder+mni to F
+    F=[F;f];
+end
+
+
+% Array for output
+DistbyBA = zeros (nFiles,4);
+
 
 %% Load files and smooth
 mxX=182; mxY=218; mxZ=182;
@@ -27,35 +46,33 @@ for i=1:nFiles
     X = find(IMGDATA<0.05); IMGDATA(X)=NaN; %%%
     % Smooth data before adding to array
     sigma = 3; % width of gaussian (assume isotropic smoothing)
-    ImageArray(:,:,:,i) = smooth3(IMGDATA,'gaussian',[sigma sigma sigma]);
+    sIMG = smooth3(IMGDATA,'gaussian',[sigma sigma sigma]);
+    %ImageArray(:,:,:,i) = smooth3(IMGDATA,'gaussian',[sigma sigma sigma]);
+    img = sIMG(2:end,2:end,2:end);
+    img(isnan(img))=0;
+    BAinIMG1 = BAmask1 & img;
+    BAinIMG2 = BAmask2 & img;
+    BAinIMG3 = BAmask3 & img;
+    BAinIMG4 = BAmask4 & img;
+    DistbyBA(i,:) = [mean(img(BAinIMG1)) mean(img(BAinIMG2)) mean(img(BAinIMG3)) mean(img(BAinIMG4))];
 end
 
-
-%% Get mean dist for each BA
-
-% load atlas
-Atlas = spm_read_vols(spm_vol('brodmann.nii'));
-BA1 = 4;  % primary motor cortex
-BA2 = 17; % primary visual cortex
-% Extract Left M1, Left V1
-% NB L hemi has x<92
-BAmask1 = ismember(Atlas,BA1); BAmask1(92:end,:,:)=false; 
-BAmask2 = ismember(Atlas,BA2); BAmask2(92:end,:,:)=false; 
-% Array for output
-DistbyBA = zeros (2,nFiles);
-
-for i=1:nFiles
-img = ImageArray(:,:,:,i); %spm_read_vols(spm_vol(imgfile));
-img = img(2:end,2:end,2:end);
-img(isnan(img))=0;
-BAinIMG1 = BAmask1 & img;
-BAinIMG2 = BAmask2 & img;
-DistbyBA(:,i) = [mean(img(BAinIMG1)) mean(img(BAinIMG2))];
-end
-% size(BAinIMG1)
-%M = [mean(img(BAmask1)) mean(img(BAmask2))];
-    
-
+%
+% %% Get mean dist for each BA
+%
+%
+% for i=1:nFiles
+% img = ImageArray(:,:,:,i); %spm_read_vols(spm_vol(imgfile));
+% img = img(2:end,2:end,2:end);
+% img(isnan(img))=0;
+% BAinIMG1 = BAmask1 & img;
+% BAinIMG2 = BAmask2 & img;
+% DistbyBA(:,i) = [mean(img(BAinIMG1)) mean(img(BAinIMG2))];
+% end
+% % size(BAinIMG1)
+% %M = [mean(img(BAmask1)) mean(img(BAmask2))];
+%
+%
 
 
 %{
@@ -75,8 +92,8 @@ Atlas = spm_read_vols(spm_vol('brodmann.nii'));
 % size(Atlas)
 % Extract Left M1, Left V1
 % NB L hemi has x<92
-BAmask1 = ismember(Atlas,BA1); BAmask1(92:end,:,:)=false; 
-BAmask2 = ismember(Atlas,BA2); BAmask2(92:end,:,:)=false; 
+BAmask1 = ismember(Atlas,BA1); BAmask1(92:end,:,:)=false;
+BAmask2 = ismember(Atlas,BA2); BAmask2(92:end,:,:)=false;
 
 
 % load mean image
