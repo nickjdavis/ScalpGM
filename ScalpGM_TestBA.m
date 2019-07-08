f7f12eae97735cdb8414660113ead6dcc66b7d02
nickjdavis <njdavis@gmail.com>
2019-07-08 12:47:01 +0100

    Changes to an increasingly unwieldy _TestBA

--- ScalpGM_TestBA.m
+++ ScalpGM_TestBA.m
@@ -1,21 +1,24 @@
 function DistbyBA = ScalpGM_TestBA (filelist)
 
-%% Load atlas
-Atlas = spm_read_vols(spm_vol('brodmann.nii'));
-BA1 = 4;  % primary motor cortex
-BA2 = 17; % primary visual cortex
-BA3 = 27; % piriform cortex
-BA4 = 46; % dlpfc
-% Extract Left M1, Left V1, Left PC
-% NB L hemi has x<92
-BAmask1 = ismember(Atlas,BA1); BAmask1(92:end,:,:)=false;
-BAmask2 = ismember(Atlas,BA2); BAmask2(92:end,:,:)=false;
-BAmask3 = ismember(Atlas,BA3); BAmask3(92:end,:,:)=false;
-BAmask4 = ismember(Atlas,BA4); BAmask4(92:end,:,:)=false;
-
-
-%% Read list of files
-T = readtable(filelist);
+%% Read file information
+% This is awful - Matlab won't allow you to create an empty table!
+% Shamefully hacky...
+T = array2table(cell(0,3));
+T.Properties.VariableNames = {'imgfolder','imgfile','MNI'};
+if iscell(filelist)
+    nLists = length(filelist);
+    for i=1:nLists
+        t = readtable(filelist{i});
+        tt = table(t.imgfolder,t.imgfile,t.MNI,'VariableNames',{'imgfolder','imgfile','MNI'});
+        if isempty(T)
+            T = tt;
+        else
+            T = union(T,tt);
+        end
+    end
+else
+    T = readtable(filelist);
+end
 nFiles = size(T,1);
 F = {};
 D = T.imgfolder;
@@ -30,8 +33,26 @@ for i=1:nFiles
 end
 
 
+%% Load atlas
+% Need a more effcicient way to do this!
+Atlas = spm_read_vols(spm_vol('brodmann.nii'));
+BA1 = 4;  % primary motor cortex
+BA2 = 17; % primary visual cortex
+BA3 = 27; % piriform cortex
+BA4 = 46; % dlpfc
+% Extract Left M1, Left V1, Left PC
+% NB L hemi has x<92
+BAmask1 = ismember(Atlas,BA1); BAmask1(92:end,:,:)=false;
+BAmask2 = ismember(Atlas,BA2); BAmask2(92:end,:,:)=false;
+BAmask3 = ismember(Atlas,BA3); BAmask3(92:end,:,:)=false;
+BAmask4 = ismember(Atlas,BA4); BAmask4(92:end,:,:)=false;
+Atlas = spm_read_vols(spm_vol('rhandnew.nii'));
+H = 255; % left hand area mask
+BAmask5 = ismember(Atlas,H);
+
+
 % Array for output
-DistbyBA = zeros (nFiles,4);
+DistbyBA = zeros (nFiles,5);
 
 
 %% Load files and smooth
@@ -54,7 +75,8 @@ for i=1:nFiles
     BAinIMG2 = BAmask2 & img;
     BAinIMG3 = BAmask3 & img;
     BAinIMG4 = BAmask4 & img;
-    DistbyBA(i,:) = [mean(img(BAinIMG1)) mean(img(BAinIMG2)) mean(img(BAinIMG3)) mean(img(BAinIMG4))];
+    BAinIMG5 = BAmask5 & img;
+    DistbyBA(i,:) = [mean(img(BAinIMG1)) mean(img(BAinIMG2)) mean(img(BAinIMG3)) mean(img(BAinIMG4)) mean(img(BAinIMG5))];
 end
 
 %
