5d32947912d51bf810e2551e570bfcf9943eebf1
nickjdavis <njdavis@gmail.com>
2019-07-16 14:04:10 +0100

    _NewMean produces a sensible-looking mean image, but the scale is wrong

--- ScalpGM_NewMean.m
+++ ScalpGM_NewMean.m
@@ -6,6 +6,16 @@ function ScalpGM_NewMean (filelist,outFileName)
 mxX=182; mxY=218; mxZ=182;
 ImageArray = zeros (mxX,mxY,mxZ);%;,nFiles);
 
+v = [];
+v.fname = strrep(outFileName,'.nii','_M.nii');
+v.dim = [mxX mxY mxZ];
+v.mat = eye(4);
+v.pinfo = [1; 0; 352]; %%% AAAUGH HACK!!!!
+v.dt = [16 0]; % float32
+outFileM = spm_create_vol(v);
+outFileM = spm_write_vol(outFileM,ImageArray);
+% spm_vol();
+% outFileM = 
 
 % read fileslist
 if iscell(filelist)
@@ -34,15 +44,32 @@ end
 
 
 
-wbstep = 100/mxZ; % step once per plane
+wbstep = 1/mxZ; % step once per plane
 wb = waitbar(0,'Reading image planes...');
 
+
+
 for plane=1:mxZ
-    pause(0.01)
+    Pstack = zeros (mxX,mxY,nFiles);
+    for imgFile = 1:nFiles
+        % open file
+        mnifilename = strcat(D{imgFile},'\',M{imgFile});
+        mnifile = spm_vol(mnifilename);
+        % read slice plane from file
+        P = spm_slice_vol(mnifile,spm_matrix([0 0 plane]),mnifile.dim(1:2),0);
+        % stack with others
+        Pstack(:,:,plane) = P;
+    end
+    % average / sd / cov
+    SliceMean = nanmean(Pstack,3);
+    %size(SliceMean)
+    % write to outfile
+    outFileM = spm_write_plane(outFileM,SliceMean,plane);
+    % update waitbar
     waitbar(plane*wbstep,wb);
 end
 close(wb)
 
 
 % create outfile
-outfile = [];
+% outfile = [];
