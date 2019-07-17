6d70a63b88ed53a0cf1f1454ac6c6aff9d487dcb
nickjdavis <njdavis@gmail.com>
2019-07-17 13:05:02 +0100

    Fleshed out _GetSubjectData a bit

--- ScalpGM_GetSubjectData.m
+++ ScalpGM_GetSubjectData.m
@@ -1,1 +1,19 @@
 function ScalpGM_GetSubjectData (subjectInfo,ScalpGMinfo)
+
+
+% load subjectInfo (comes with OASIS)
+infotable = readtable(subjectInfo,'text','Delimiter',',');
+
+% load ScalpGMinfo (from previous processing steps)
+    T = readtable(ScalpGMinfo);
+    nFiles = size(T,1);
+
+
+% for each file in T, locate info in infotable
+for i=1:nFiles
+    % parse filename
+    % find line matching subject number (some may be in twice)
+    % get [sex age eTIV nWBV]
+    % add info to table
+end
+% save table back into original file
