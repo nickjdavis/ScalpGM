f1b6a91563b5bb895e5b434d3a5c96b5b51ded64
nickjdavis <njdavis@gmail.com>
2019-07-19 17:23:58 +0100

    Adding _DepthCorr. Not working yet.

---
+++ ScalpGM_DepthCorr.m
@@ -0,0 +1,99 @@
+function ScalpGM_DepthCorr (filelist, ROIfile, outFileName)
+
+mxX=182; mxY=218; mxZ=182;
+
+
+% Get data
+T = readtable(filelist);
+nFiles = size(T,1);
+disp(strcat('Found  ',num2str(nFiles),' files.'))
+F = {};
+D = T.imgfolder;
+I = T.imgfile;
+M = T.MNI;
+for i=1:nFiles
+    % get folder
+    %[p,n,e]=fileparts(I{i});
+    p = D{i};
+    % get MNI file
+    f=strcat(p,'\',M{i});
+    % add folder+mni to F
+    F=[F;f];
+end
+
+% Open ROIfile
+ROIs = spm_read_vols(spm_vol(ROIfile));
+
+%% Get M1 depth
+disp ('Calculating depth of target region...')
+M1all = zeros(nFiles,1);
+
+wbstep = 1/nFiles; % step once per plane
+wb = waitbar(0,'Estimating target depth...');
+
+%V = ismember(ROIfile,1);
+V = ismember(ROIs,1); % 1 for M1
+ROI = [];
+ROI.label = 'xxx';
+ROI.file = ROIfile;
+ROI.vol = V;
+%ROIarray = [ROIarray ROI];
+
+
+for i=1:nFiles
+    mnifile = F{i};
+    disp(mnifile)
+    V = spm_vol(mnifile);
+    IMGDATA = spm_read_vols(V);
+    X = find(IMGDATA<0.05); IMGDATA(X)=NaN; %%%
+    % Smooth data before adding to array
+    sigma = 3; % width of gaussian (assume isotropic smoothing)
+    sIMG = smooth3(IMGDATA,'gaussian',[sigma sigma sigma]);
+    img = sIMG(2:end,2:end,2:end);
+    img(isnan(img))=0;
+    % size(img)
+    % size(ROI.vol)
+    Y = ROI.vol & img;
+    Dpth = mean(img(Y));
+    M1all(i) = Dpth;
+    waitbar(i*wbstep,wb);
+end
+close(wb)
+% mean(M1all)
+% hist(M1all)
+
+%% Correlate all depths with M1 depth
+disp ('Correlating whole brain with target area...')
+wbstep = 1/mxZ; % step once per plane
+wb = waitbar(0,'Reading image planes...');
+
+for plane=1:mxZ
+    Pstack = zeros (mxX,mxY,nFiles);
+    for imgFile = 1:nFiles
+        % open file
+        d = D{imgFile}
+        m = M{imgFile}
+        %mnifilename = strcat(D{imgFile},'\',M{imgFile});
+        mnifilename = strcat(d,'\',m);
+        mnifile = spm_vol(mnifilename);
+        % read slice plane from file
+        P = spm_slice_vol(mnifile,spm_matrix([0 0 plane]),mnifile.dim(1:2),0);
+        % stack with others, setting values less than 0.05 to NaN
+        X = find(P<0.05); P(X)=NaN; %%%
+        Pstack(:,:,imgFile) = P;
+    end
+    % average / sd / cov
+    % DO CORR HERE
+%     SliceMean = nanmean(Pstack,3);
+%     SliceSD = nanstd(Pstack,[],3);
+%     SliceCoV= SliceSD./SliceMean;
+    % write to outfile
+    outFileM  = spm_write_plane(outFileM,SliceMean,plane);
+    outFileSD = spm_write_plane(outFileSD,SliceSD,plane);
+    outFileCoV= spm_write_plane(outFileCoV,SliceCoV,plane);
+    % update waitbar
+    waitbar(plane*wbstep,wb);
+end
+close(wb)
+
+%% Write output file
--- ScalpGM_MakeROIs.m
+++ ScalpGM_MakeROIs.m
@@ -6,6 +6,7 @@ function ScalpGM_MakeROIs ()
 BA = spm_vol('brodmann.nii');
 mxX=182; mxY=218; mxZ=182;
 ImageArray = zeros(mxX-1,mxY-1,mxZ-1);
+offset = [90 125 71]; % offset of origin
 
 % add L M1
 M1vol = spm_vol('.\ROIs\rhandnew.nii');
@@ -25,6 +26,13 @@ CCvol = spm_vol('.\ROIs\rR-CogControl.nii');
 V = spm_read_vols(CCvol);
 X = 3*(V>0.5);
 ImageArray = ImageArray+X;
+
+
+% expt - sphere for left V1
+ctr = [-12 -102 6];
+offsetctr = ctr+offset;
+ImageArray(offsetctr(1),offsetctr(2),offsetctr(3))=99;
+
 
 
 % create new volume
