15a68ca73e596e64b98a0c6d77522ff2fbe3a21b
nickjdavis <njdavis@gmail.com>
2019-08-02 18:31:59 +0100

    Working on _TestBA. Using AAL atlas. Mostly working but returning zeros at mean stage, so need to fix this.

--- ScalpGM_TestBA.m
+++ ScalpGM_TestBA.m
@@ -1,4 +1,5 @@
-function DistbyBA = ScalpGM_TestBA (filelist)
+function DistbyArea = ScalpGM_TestBA (filelist)
+
 
 %% Read file information
 % This is awful - Matlab won't allow you to create an empty table!
@@ -35,48 +36,66 @@ end
 
 %% Load atlas
 % Need a more effcicient way to do this!
-Atlas = spm_read_vols(spm_vol('brodmann.nii'));
-BA1 = 4;  % primary motor cortex
-BA2 = 17; % primary visual cortex
-BA3 = 27; % piriform cortex
-BA4 = 46; % dlpfc
-% Extract Left M1, Left V1, Left PC
-% NB L hemi has x<92
-BAmask1 = ismember(Atlas,BA1); BAmask1(92:end,:,:)=false;
-BAmask2 = ismember(Atlas,BA2); BAmask2(92:end,:,:)=false;
-BAmask3 = ismember(Atlas,BA3); BAmask3(92:end,:,:)=false;
-BAmask4 = ismember(Atlas,BA4); BAmask4(92:end,:,:)=false;
-Atlas = spm_read_vols(spm_vol('rhandnew.nii'));
-H = 255; % left hand area mask
-BAmask5 = ismember(Atlas,H);
+% Atlas = spm_read_vols(spm_vol('brodmann.nii'));
+% BA1 = 4;  % primary motor cortex
+% BA2 = 17; % primary visual cortex
+% BA3 = 27; % piriform cortex
+% BA4 = 46; % dlpfc
+% % Extract Left M1, Left V1, Left PC
+% % NB L hemi has x<92
+% BAmask1 = ismember(Atlas,BA1); BAmask1(92:end,:,:)=false;
+% BAmask2 = ismember(Atlas,BA2); BAmask2(92:end,:,:)=false;
+% BAmask3 = ismember(Atlas,BA3); BAmask3(92:end,:,:)=false;
+% BAmask4 = ismember(Atlas,BA4); BAmask4(92:end,:,:)=false;
+% Atlas = spm_read_vols(spm_vol('rhandnew.nii'));
+% H = 255; % left hand area mask
+% BAmask5 = ismember(Atlas,H);
+
+
+Atlas = spm_read_vols(spm_vol('rROI_MNI_V4.nii')); % resliced version
+fid = fopen('ROI_MNI_V4.txt');
+Labels = textscan(fid,'%s\t%s\t%d');
+fclose (fid);
+nLabels = length(Labels{1})
 
 
 % Array for output
-DistbyBA = zeros (nFiles,5);
+DistbyArea = zeros (nFiles,nLabels);
 
 
 %% Load files and smooth
 mxX=182; mxY=218; mxZ=182;
-ImageArray = zeros (mxX,mxY,mxZ,nFiles);
+% mxX=91; mxY=109; mxZ=91;
+% ImageArray = zeros (mxX,mxY,mxZ,nFiles);
 disp('Adding image files...')
 for i=1:nFiles
     mnifile = F{i};
     disp(mnifile)
     V = spm_vol(mnifile);
     IMGDATA = spm_read_vols(V);
-    X = find(IMGDATA<0.05); IMGDATA(X)=NaN; %%%
+    %mean(mean(mean(IMGDATA)))
+    %X = find(IMGDATA<0.05); IMGDATA(X)=0; %%% was NaN
+    %%size(find(isnan(IMGDATA)))
     % Smooth data before adding to array
-    sigma = 3; % width of gaussian (assume isotropic smoothing)
-    sIMG = smooth3(IMGDATA,'gaussian',[sigma sigma sigma]);
+%     sigma = 3; % width of gaussian (assume isotropic smoothing)
+%     sIMG = smooth3(IMGDATA,'gaussian',[sigma sigma sigma]);
     %ImageArray(:,:,:,i) = smooth3(IMGDATA,'gaussian',[sigma sigma sigma]);
-    img = sIMG(2:end,2:end,2:end);
-    img(isnan(img))=0;
-    BAinIMG1 = BAmask1 & img;
-    BAinIMG2 = BAmask2 & img;
-    BAinIMG3 = BAmask3 & img;
-    BAinIMG4 = BAmask4 & img;
-    BAinIMG5 = BAmask5 & img;
-    DistbyBA(i,:) = [mean(img(BAinIMG1)) mean(img(BAinIMG2)) mean(img(BAinIMG3)) mean(img(BAinIMG4)) mean(img(BAinIMG5))];
+%     img = sIMG(2:end,2:end,2:end);
+    img = IMGDATA;
+    %mean(mean(mean(img)))
+    %size(isnan(img))
+    img(find(isnan(img)))=0;
+    %mean(mean(mean(img)))
+    meanDepths = zeros(nLabels,1);
+    for m=1:nLabels
+        %l = Labels{3}(m)
+        Mask = ismember(Atlas,Labels{3}(m));
+        %size(Mask)
+        BrainInMask = Mask & img;
+        %disp(length(find(BrainInMask)))
+        meanDepths(m) = mean(img(BrainInMask))
+    end
+    DistByArea(i,:) = meanDepths;
 end
 
 %
