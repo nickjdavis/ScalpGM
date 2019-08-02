8b91d3d75b7619d50360621f31e344d562107b43
nickjdavis <njdavis@gmail.com>
2019-08-02 07:21:15 +0100

    _ViewCH plots convex hull from scalp data

--- ScalpGM_ViewCH.m
+++ ScalpGM_ViewCH.m
@@ -1,3 +1,34 @@
 % View convex hulls to check
 
-function ScalpGM_ViewCH ()
+function ScalpGM_ViewCH (filelist)
+
+
+% Get data
+T = readtable(filelist);
+nFiles = size(T,1);
+disp(strcat('Found  ',num2str(nFiles),' files.'))
+F = {};
+D = T.imgfolder;
+I = T.imgfile;
+S = T.scalp;
+G = T.GM;
+%M = T.MNI;
+% for i=1:nFiles
+%     % get folder
+%     %[p,n,e]=fileparts(I{i});
+%     p = D{i};
+%     % get MNI file
+%     f=strcat(p,'\',M{i});
+%     % add folder+mni to F
+%     F=[F;f];
+% end
+
+
+for i=1:nFiles
+    f = strcat(D{i},'\',S{i});
+    scalp_points = ScalpGM_getCH3d (f);
+    plot3(scalp_points(:,1),scalp_points(:,2),scalp_points(:,3),'.')
+    %trisurf([scalp_points; scalp_points(1,:)])
+    %waitforbuttonpress
+end
+
