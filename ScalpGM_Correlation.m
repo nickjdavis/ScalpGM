a9ba1a89cca7337244de5e636ecb41d1d0d8e623
nickjdavis <njdavis@gmail.com>
2020-04-15 14:15:54 +0100

    Added new correlation function

---
+++ ScalpGM_Correlation.m
@@ -0,0 +1,10 @@
+% ScalpGM_Correlation
+%
+% Computes the correlation across images of the whole brain with a
+% specific point
+% 
+% filelist: table of files for processing
+% ROIinfo : [x y z r] is MNI coord of point, plus radius
+%
+
+function ScalpGM_Correlation (filelist,ROIinfo)
