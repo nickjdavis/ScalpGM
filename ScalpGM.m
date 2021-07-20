d27e5c89aaff4b18c8a563906e760d949875dc2b
Nick Davis <n.davis@mmu.ac.uk>
2021-07-20 14:46:49 +0100

    Minor text changes

--- ScalpGM.m
+++ ScalpGM.m
@@ -73,15 +73,15 @@ for i=1:n
     T1file = I{i}; 
     % Check here for .nii.gz instead of .nii
     if isempty(dir(strcat(T1folder,'\',T1file)))
-        disp(sprintf('T1 file %s not found',T1file))
+        disp(sprintf('WARNING - T1 file %s not found',T1file))
         T1gz = [T1file '.gz'];
         if ~isempty(dir(strcat(T1folder,'\',T1gz)))
-            disp(sprintf('Found GZip file %s. Attempting to extract.',T1gz))
+            disp(sprintf('-- Found GZip file %s. Attempting to extract.',T1gz))
             gunzip(strcat(T1folder,'\',T1gz));
             if isempty(dir(strcat(T1folder,'\',T1file)))
-                disp(sprintf('File %s still not present.',T1file))
+                disp(sprintf('-- File %s still not present.',T1file))
             else
-                disp(sprintf('File %s successfully extracted.',T1file))
+                disp(sprintf('-- File %s successfully extracted.',T1file))
             end
         end
     end
