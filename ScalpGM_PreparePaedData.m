abac1af4b9f5c33b472b874a322a89457498659b
nickjdavis <njdavis@gmail.com>
2025-02-24 16:29:56 +0000

    New branch for the Paediatric datset

---
+++ ScalpGM_PreparePaedData.m
@@ -0,0 +1,13 @@
+function ScalpGM_PreparePaedData(folder)
+% Preprocessing steps for the paediatric dataset
+% 1. [...]
+
+% Is the subject folder, or folder of folders?
+d = dir(folder)
+for i=1:length(d)
+    if d(i).isdir
+        % check if is an OAS dir
+    end
+end
+
+% STEP 1. Locate folder with T1
